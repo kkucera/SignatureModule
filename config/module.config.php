40e4a6ace9f0bea1d11bfebd207308ebb1397b14
Kevin Kucera <kevin@webpt.com>
2014-09-01 17:19:38 -0700

    Added layout and view with basic sig form.

--- config/module.config.php
+++ config/module.config.php
@@ -59,6 +59,9 @@ return array(
     ),
 
     'view_manager' => array(
+        'template_map' => array(
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+        ),
         'template_path_stack' => array(
             'SignatureModule' => __DIR__ . '/../view',
         ),
